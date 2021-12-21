60f641cf42196aeeb4b876682e5b70d16bdc271b
jfranco9202 <jeremy.franco41@yahoo.com>
2021-12-21 11:33:04 -0600

    Methods Exercise 2

---
+++ Methods Exercise 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Methods_Exercise_2
+{
+    class Program
+    {
+        public static double GetArea()
+        {
+            Console.WriteLine("What is the radius of the circle?");
+            var radius = double.Parse(Console.ReadLine());
+            return Math.PI* radius *radius;
+
+        }
+    }
+}
